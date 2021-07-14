870f467ff81c879a486cdd482d97a44fcbc561d4
Stephen Chu <chu.stephen@gmail.com>
2021-07-13 22:03:27 -0700

    add some display boxes

--- src/pages/SearchDetail/index.tsx
+++ src/pages/SearchDetail/index.tsx
@@ -1,15 +1,18 @@
 import { useContractKit } from "@celo-tools/use-contractkit";
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "theme-ui";
 import NOM from "src/abis/nomspace/Nom.json";
-import { AbiItem, hexToBytes } from "web3-utils";
+import { AbiItem } from "web3-utils";
 import { Nom } from "src/generated/Nom";
 import { ethers } from "ethers";
 
 export const SearchDetail: React.FC = () => {
   const { name } = useParams<{ name: string }>();
   const { kit } = useContractKit();
+
+  const [resolution, setResolution] = useState("");
+  const [owner, setOwner] = useState("");
 
   const nom = new kit.web3.eth.Contract(
     NOM.abi as AbiItem[],
@@ -20,17 +23,28 @@ export const SearchDetail: React.FC = () => {
     nom.methods
       .resolve(ethers.utils.formatBytes32String(name))
       .call()
-      .then((resolution: string) => console.log("Resolution: ", resolution));
+      .then((resolution: string) => {
+        console.log("Resolution: ", resolution);
+        setResolution(resolution);
+      });
 
     nom.methods
       .nameOwner(ethers.utils.formatBytes32String(name))
       .call()
       .then((owner: string) => {
         console.log("Owner: ", owner);
-        const reserved = parseInt(owner, 16) != 0;
+        setOwner(owner);
+        const reserved = parseInt(owner, 16) !== 0;
         console.log("Reserved: ", reserved);
       });
-  }, []);
+  }, [name, nom.methods]);
 
-  return <Box>Search {name}</Box>;
+  return (
+    <div>
+      <Box>Search {name}</Box>
+      <Box>Resolution: {resolution}</Box>
+      <Box>Owner: {owner}</Box>
+      <Box>Can be reserved: {parseInt(owner, 16) !== 0 ? "True" : "False"}</Box>
+    </div>
+  );
 };
