a4706bc8f79c1cdef024972359722e0a412b8101
Brian Li <brnli7@gmail.com>
2022-01-12 15:14:51 -0800

    Fix sidebar color

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -543,7 +543,8 @@ export const preset = {
         color: "textColor",
         minWidth: [200, 300, 430],
         padding: "24px 38px",
-        minHeight: "fit-content",
+        height: "fit-content",
+        minHeight: "100%",
       },
       walletContainer: { marginTop: 20 },
       heading: {
