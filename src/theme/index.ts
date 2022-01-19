1377949274ed9b334e2dac01495733725b284ad1
Brian Li <brnli7@gmail.com>
2022-01-18 18:21:34 -0800

    Fix sidebar

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -602,7 +602,6 @@ export const preset = {
         minWidth: [200, 300, 430],
         padding: "24px 38px",
         minHeight: "fit-content",
-        height: "100%",
       },
       walletContainer: { marginTop: 20 },
       heading: {
