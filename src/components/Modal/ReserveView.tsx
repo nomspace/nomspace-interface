87ff0c295ee46d8dca19a3ce359e08c2c9282eb2
chiumax <dumblole@gmail.com>
2022-02-05 20:02:00 -0500

    nomvoucher works on reserve now, counter updates when changing metamask network automatically

--- src/components/Modal/ReserveView.tsx
+++ src/components/Modal/ReserveView.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { GlobalNom } from "hooks/useNom";
-import { Box, Button, Flex, Input, Spinner, Text } from "theme-ui";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Spinner,
+  Text,
+  Image,
+  ThemeProvider as ThemeUIThemeProvider,
+  useColorMode,
+} from "theme-ui";
 import { useUSD } from "hooks/useUSD";
 import { useReserve } from "hooks/useReserve";
 import { formatUnits } from "ethers/lib/utils";
@@ -9,20 +19,66 @@ import { getNomCost, getNomYears } from "utils/cost";
 import { useContractKit } from "@celo-tools/use-contractkit";
 import { BlockscoutAddressLink } from "components/BlockscoutAddressLink";
 import { shortenAddress } from "utils/address";
+import { TOKEN_LIST, Token } from "addresses";
+import MenuItem from "@mui/material/MenuItem";
+import { Select } from "@mui/material";
+import theme from "theme";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 interface Props {
   name: string;
 }
 
 export const ReserveView: React.FC<Props> = ({ name }) => {
-  const { address } = useContractKit();
+  const { address, network } = useContractKit();
 
   const [nom] = GlobalNom.useContainer();
   const [years, setYears] = React.useState(1);
   const [cost, setCost] = React.useState(name.length <= 3 ? 20 : 5);
-  const [usd, refetchUSD] = useUSD();
+  const [coin, setCoin] = React.useState<Token | undefined>();
+  const [usd, refetchUSD] = useUSD(coin?.address);
   const [tokens] = useTokens();
   const { reserve, loading } = useReserve(name);
+
+  const [colorMode] = useColorMode();
+
+  let button;
+  React.useEffect(() => {
+    if (TOKEN_LIST) {
+      let sameChainToken = Object.entries(TOKEN_LIST).filter(
+        ([_, t]) => t.chainId == network.chainId
+      );
+
+      if (sameChainToken[0]) {
+        setCoin(sameChainToken[0][1]);
+      }
+    }
+  }, [network]);
+
+  React.useEffect(() => {
+    console.log("coin", coin);
+  }, [coin]);
+
+  // when dropdown updated, update cost
+  React.useEffect(() => {
+    setCost(getNomCost(name, years, coin?.symbol == "vNOM"));
+    console.log("hook", usd);
+    if (usd) {
+      console.log(
+        "bal",
+        Number(formatUnits(usd.balance, usd.decimals)).toFixed(4)
+      );
+    }
+    if (usd && Number(formatUnits(usd.balance, usd.decimals)) < cost) {
+      button = (
+        <Button variant="modal.form.submit" disabled={true}>
+          Insufficient funds
+        </Button>
+      );
+    } else if (usd) {
+      console.log(Number(formatUnits(usd.balance, usd.decimals)), cost);
+    }
+  }, [usd]);
 
   if (nom == null) {
     return <Spinner />;
@@ -32,7 +88,7 @@ export const ReserveView: React.FC<Props> = ({ name }) => {
     <Button
       variant="modal.form.submit"
       onClick={async () => {
-        await reserve(years);
+        await reserve(years, coin?.name || "");
         refetchUSD();
       }}
     >
@@ -40,7 +96,8 @@ export const ReserveView: React.FC<Props> = ({ name }) => {
     </Button>
   );
 
-  let button = confirmButton;
+  button = confirmButton;
+
   if (usd) {
     if (Number(formatUnits(usd.balance, usd.decimals)) < cost) {
       button = (
@@ -60,6 +117,73 @@ export const ReserveView: React.FC<Props> = ({ name }) => {
         </Text>
       </Text>
       <Box variant="modal.form.container">
+        <Box variant="modal.form.selectWrapper">
+          <ThemeProvider
+            theme={createTheme({
+              palette: {
+                mode: `${colorMode === "light" ? "light" : "dark"}`,
+              },
+            })}
+          >
+            <Select
+              MenuProps={{
+                disableScrollLock: true,
+              }}
+              value={coin?.name || ""}
+              label="Select Currency"
+              onChange={(e) => {
+                const token = TOKEN_LIST[e.target.value] || coin;
+                setCoin(token);
+              }}
+              autoWidth
+              sx={{
+                borderRadius: "11px",
+                backgroundColor: "var(--theme-ui-colors-secondaryBackground)",
+                border: "4px solid var(--theme-ui-colors-primary)",
+                // backgroundColor: "white",
+                filter: "drop-shadow(0px 3px 6px #00000029)",
+                width: "100%",
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+              }}
+            >
+              {TOKEN_LIST &&
+                Object.entries(TOKEN_LIST)
+                  .filter(([_, t]) => t.chainId == network.chainId)
+                  .map(([_, t]) => {
+                    return (
+                      <MenuItem value={t.name} key={t.address}>
+                        <ThemeUIThemeProvider theme={theme}>
+                          <Flex
+                            sx={{
+                              justifyContent: "space-between",
+                              alignItems: "center",
+                              width: "100%",
+                            }}
+                          >
+                            <Flex
+                              sx={{
+                                alignItems: "center",
+                                fontFamily: "sen",
+                                fontSize: ["25px", null, null, "33px"],
+                              }}
+                            >
+                              <Image
+                                sx={{ height: 24, width: 24 }}
+                                mr={4}
+                                src={t.logoURI}
+                              />
+                              {t.name}
+                            </Flex>
+                          </Flex>
+                        </ThemeUIThemeProvider>
+                      </MenuItem>
+                    );
+                  })}
+            </Select>
+          </ThemeProvider>
+        </Box>
         <Flex
           variant="modal.form.durationWrapper"
           sx={{ alignItems: "center" }}
@@ -72,7 +196,7 @@ export const ReserveView: React.FC<Props> = ({ name }) => {
               const years = Number(e.target.value);
               if (isNaN(years)) return;
               setYears(years);
-              setCost(getNomCost(name, years));
+              setCost(getNomCost(name, years, coin?.symbol == "vNOM"));
             }}
             mr={2}
           />
@@ -108,12 +232,10 @@ export const ReserveView: React.FC<Props> = ({ name }) => {
               const cost = Number(e.target.value);
               if (isNaN(cost)) return;
               setCost(cost);
-              setYears(getNomYears(name, cost));
+              setYears(getNomYears(name, cost, coin?.symbol == "vNOM"));
             }}
           />
-          <Text sx={{ ml: 8 }}>
-            {tokens?.find((t) => t.address === usd?.address)?.symbol ?? "USD"}
-          </Text>
+          <Text sx={{ ml: 8 }}>{coin?.symbol}</Text>
         </Flex>
         {loading ? (
           <Spinner />
