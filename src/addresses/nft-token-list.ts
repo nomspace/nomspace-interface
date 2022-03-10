9af1aac3a25be8410ee5edef86d1ced5df6a54be
Zhethan <40449360+Zhethan@users.noreply.github.com>
2022-03-09 20:27:51 -0500

    added ChinChilla Gang and Celostrials NFTs (#16)
    * added ChinChilla Gang and Celostrials NFTs
    
    * Added Daopolis

--- src/addresses/nft-token-list.ts
+++ src/addresses/nft-token-list.ts
@@ -1,9 +1,9 @@
 export type NFT = {
-  address: string;
-  name: string;
-  chainId: number;
-  imagePrefix?: string;
-  imageExt?: string;
+  address: string,
+  name: string,
+  chainId: number,
+  imagePrefix?: string,
+  imageExt?: string,
 };
 
 const nftTokenList: Record<number, NFT[]> = {
@@ -68,6 +68,21 @@ const nftTokenList: Record<number, NFT[]> = {
       imagePrefix:
         "https://ipfs.io/ipfs/QmTDzVspMNFLEQ2QAUwRBkEb1aTdxZFwU2JE7z6DmnMipE/",
       imageExt: "jpg",
+    },
+    {
+      address: "0xc8DF51073CD581902b4fb50131d31f29343131F0",
+      name: "ChinChilla Gang",
+      chainId: 42220,
+    },
+    {
+      address: "0xAc80c3c8b122DB4DcC3C351ca93aC7E0927C605d",
+      name: "Celostrials",
+      chainId: 42220,
+    },
+    {
+      address: "0xc4ea80deCA2415105746639eC16cB0cF8378996A",
+      name: "Daopolis",
+      chainId: 42220,
     },
   ],
   44787: [
